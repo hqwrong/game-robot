baca8f8c5164788a6cfbeafd93b2d4a60801c328
hqwrong <hq.wrong@gmail.com>
2015-04-02 13:46:20 +0800

    modify readme

--- README.md
+++ README.md
@@ -9,18 +9,25 @@ A simple robot client for game server.
 2. Two modes support. `client` mode for interaction on command line, `simulator` mode for async running which 
    is often used in pressure test.
 
+## Launch
+You need a config file to launch. see `test/config.py` for detail.
+
+## Game logic
+Please add your logic functions in `game.py`.
+
+use `@addcmd(ARGS, COMMAND_NAME)` decorator to add client command. (Uncomment `mode = "client"` in `test/config.py` to test)
+
+use `@addhandle(protoname)` decorator to add handle function for game server's request.
 
 ## Test
 
-Run
+To launch test server:
 
     python -m test/server  sproto
     
-to launch test server
 
-Run
+To launch client:
 
     python main.py test/config.py
     
-to launch client.
 
--- test/config.py
+++ test/config.py
@@ -1,17 +1,34 @@
-# proto = "protobuf"              # protobuf/sproto
-# proto_path = "./data/pb"       # ./data/pb or ./data/sp
+#### game server address
+server = "127.0.0.1"
+port   = 8251
 
+
+############# config proto
+
+##  protobuf
+# proto = "protobuf"              # protobuf/sproto
+# proto_path = "./data/pb"        # ./data/pb or ./data/sp
+
+## sproto
 proto = "sproto"
 proto_path = ["./test/test.spb", "./test/test.spb"]
 proto_header = "header"
 
+############
+
+########### config mode
+
+## simulator mode
 mode = "simulator"                 # client / simulator
-server = "127.0.0.1"
-port   = 8251
 run = [
     {
         "cmd" : "echo",
         "args" : ["hello world!"],
-        "count" : 1,
+        "count" : 1,            # how many clients to launch to run this command
     },
 ]
+
+## client mode
+# mode = "client"
+
+############
